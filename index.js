0d74bc92ec0a041bbeebb0bc56403c9f638d8f6f
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-10-25 19:05:46 +0600

    added category id and data and filter them for client site

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
-
+const courses = require('./data/coursesdetails.json')
 app.get('/', (req,res)=>{
   res.send('Courses API Running');
 });
@@ -14,6 +14,20 @@ app.get('/', (req,res)=>{
 app.get('/courses-categories',(req,res)=>{
   res.send(categories)
 });
+
+app.get('/category/:id',(req,res)=>{
+   const id = req.params.id;
+   const categoryCourses = courses.filter(course=>course.category_id === id);
+   res.send(categoryCourses);
+})
+
+app.get('/courses/:id',(req,res)=>{
+  const id = req.params.id;
+  const chosenCourses =courses.find(course => course._id === id);
+    res.send(chosenCourses);
+  
+  console.log(req.params);
+})
 
 app.listen(port, ()=>{
   console.log('Dragon News Server Running', port);
